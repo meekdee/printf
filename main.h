4cb627842c1db57fa72e9f64aca03a93efc2e257
Ramilmikha <112623334+Ramilmikha@users.noreply.github.com>
2023-10-17 15:22:11 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,23 @@
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+    char fmt;
+    int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
